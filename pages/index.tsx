abda2bf5f413a970cf637b95c5fb34d69d889454
JoDMsoluth <jodmsoluth@gmail.com>
2022-12-12 10:36:13 +0900

    Feat: add first scene

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
+import {PerspectiveCamera, Stats} from "@react-three/drei";
 import {Canvas} from "@react-three/fiber";
 import AnimatedBox from "../components/AnimatedBox";
 import CameraOrbitController from "../components/CameraOrbitController";
 
 export default function Home() {
+  const testing = true;
+
   return (
     <div className='container'>
       <Canvas>
+        <PerspectiveCamera makeDefault />
+        {testing ? <Stats /> : null}
+        {testing ? <axesHelper args={[2]} /> : null}
+        {testing ? <gridHelper args={[10, 10]} /> : null}
         <CameraOrbitController />
         <ambientLight intensity={0.1} />
         <directionalLight color='red' position={[0, 0, 5]} />
-        <AnimatedBox />
+        <AnimatedBox isTesting={testing} />
       </Canvas>
     </div>
   );
