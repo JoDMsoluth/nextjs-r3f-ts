2eb90a6b172cd73317a978dcac9829f9340dae6b
JoDMsoluth <jodmsoluth@gmail.com>
2022-12-14 17:05:29 +0900

    Feat: add Catmullrom curve traverse

--- components/Object/Gltf/Pterosaur.tsx
+++ components/Object/Gltf/Pterosaur.tsx
@@ -1,58 +1,42 @@
 import * as THREE from "three";
-import { useAnimations, useGLTF } from "@react-three/drei";
-import { useFrame, useThree } from "@react-three/fiber";
+import {CatmullRomLine, useAnimations, useGLTF} from "@react-three/drei";
+import {useFrame} from "@react-three/fiber";
 
-import { useEffect, useMemo, useState } from "react";
+import {useEffect, useMemo, useRef, useState} from "react";
 
 export default function Pterosaur(props: JSX.IntrinsicElements["mesh"]) {
-  const { scene, animations } = useGLTF(
-    "/gltf/dsungaripterus/source/model.gltf"
-  );
+  const group = useRef<THREE.Group>(new THREE.Group());
+  const {scene, animations} = useGLTF("/gltf/dsungaripterus/source/model.gltf");
 
-  const { ref, actions, names, ...gldd } = useAnimations(animations);
+  const {ref, actions, names} = useAnimations(animations);
 
   const [index] = useState(1);
 
   const path = useMemo(
     () =>
       new THREE.CatmullRomCurve3([
-        new THREE.Vector3(2000, -1100, -3000),
-        new THREE.Vector3(-500, -900, 500),
-        new THREE.Vector3(-1500, -800, -1500),
-        new THREE.Vector3(1500, -700, -1500),
-
-        new THREE.Vector3(1500, -600, 1500),
-        new THREE.Vector3(-1500, -500, 1500),
-        new THREE.Vector3(-1500, -400, -1500),
-        new THREE.Vector3(1500, -300, -1500),
-
-        new THREE.Vector3(1500, -200, 1500),
-        new THREE.Vector3(-1500, 1500, 3500),
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(-8, 6, -5),
+        new THREE.Vector3(-2, 3, 7),
+        new THREE.Vector3(6, 4.5, 3),
+        new THREE.Vector3(0.5, 8, -1),
       ]),
     []
   );
 
-  const points = path.getPoints(1000);
-  const geometry = new THREE.BufferGeometry().setFromPoints(points);
-  const material = new THREE.LineBasicMaterial({ color: 0x000000 });
-  const pathLine = new THREE.Line(geometry, material);
+  useFrame((state, delta) => {
+    const {clock} = state;
+    const time = clock.oldTime;
 
-  const { scene: renderrer } = useThree();
+    const currentPosition = new THREE.Vector3();
+    const nextPosition = new THREE.Vector3();
 
-  const clock = new THREE.Clock();
-  const time = clock.oldTime;
+    path.getPointAt(time % 1, currentPosition);
+    path.getPointAt((time + 0.001) % 1, nextPosition);
 
-  renderrer.add(pathLine);
-
-  // useFrame((state, delta) => {
-  //   const currentPosition = new THREE.Vector3();
-  //   const nextPosition = new THREE.Vector3(3);
-
-  //   path.getPointAt(time % 1, currentPosition);
-  //   path.getPointAt((time + 0.001) % 1, nextPosition);
-
-  //   ref.current?.copy(new THREE.Vector3(currentPosition));
-  // });
+    ref.current?.position.copy(currentPosition);
+    ref.current?.lookAt(nextPosition.x, nextPosition.y, nextPosition.z);
+  });
 
   useEffect(() => {
     actions[names[index]]?.reset().fadeIn(0.5).play();
@@ -63,8 +47,28 @@ export default function Pterosaur(props: JSX.IntrinsicElements["mesh"]) {
   }, [index, actions, names]);
 
   return (
-    <mesh ref={ref as any} {...props}>
-      <primitive object={scene} />
-    </mesh>
+    <>
+      <CatmullRomLine
+        points={[
+          new THREE.Vector3(0, 0, 0),
+          new THREE.Vector3(-8, 6, -5),
+          new THREE.Vector3(-2, 3, 7),
+          new THREE.Vector3(6, 4.5, 3),
+          new THREE.Vector3(0.5, 8, -1),
+        ]}
+        closed={false}
+        curveType={"centripetal"}
+        tension={0.5}
+        segments={20}
+        color={"red"}
+        lineWidth={3}
+        dashed={true}
+      />
+      <group ref={group} dispose={null}>
+        <group ref={ref as any}>
+          <primitive object={scene} {...props} />
+        </group>
+      </group>
+    </>
   );
 }
